0907ced6fa593a9a57175203f54cf990d1e24bdb
Sovan Jana <sovanj@daytoday.health>
2021-04-30 16:26:09 +0530

    minor update

--- src/ClickCounter.jsx
+++ src/ClickCounter.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import withCounter from "./withCounter";
 
 function ClickCounter({ count, countHandler }) {
-  return <button onClick={countHandler}>Clicked {count} times</button>;
+  return (
+    <div>
+      <span>Click Counter: </span>
+      <button onClick={countHandler}>Click</button>
+      <span> {count} times</span>
+    </div>
+  );
 }
 
 export default withCounter(ClickCounter);
--- src/HoverCounter.jsx
+++ src/HoverCounter.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import withCounter from "./withCounter";
 
 function HoverCounter({ count, countHandler }) {
-  return <h2 onMouseOver={countHandler}>Clicked {count} times</h2>;
+  return (
+    <div style={{ backgroundColor: "lightgrey" }}>
+      <p onMouseOver={countHandler}>Hover Counter: Hovered {count} times</p>
+    </div>
+  );
 }
 
 export default withCounter(HoverCounter);
